7d00153879793635ef56ee718c832dcc64275dd8
roman <roman.kozhukhivskyi@digitalcareerinstitute.org>
2018-09-25 15:12:36 +0200

    working setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-    module: {
-        rules: [
-          {
-              test: /\.scss$/,
-              use: ['style-loader', 'css-loader','sass-loader']
-          }
-      ]
-    }
+   module: {
+       rules: [
+           { // css / sass / scss loader for webpack
+               test: /\.scss$/,
+               use: ExtractTextPlugin.extract({
+                   use: ['css-loader', 'sass-loader' ],
+               })
+           }
+       ]
+   },
+   plugins: [
+       new ExtractTextPlugin({ // define where to save the file
+           filename: '../css/style.css',
+           allChunks: true,
+       }),
+   ],
 };
